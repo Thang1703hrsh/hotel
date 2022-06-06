bef5935f74527be7683e867e8909096000165709
Taka304 <hoangduoclam30@gmail.com>
2022-06-06 14:29:00 +0700

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,17 @@ const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 const path = require('path')
-
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors());
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/rooms', require('./routes/roomRoutes'))
+
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')))
