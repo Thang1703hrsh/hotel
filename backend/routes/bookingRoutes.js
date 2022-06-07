319f42a8462ad49bfbc3c9db3a41501e6dfcb1a0
tonyvu1289 <tonyvu1289@gmail.com>
2022-06-07 16:48:23 +0700

    fix pay money

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const {
     createBooking,
     unpaidBooking,
+    paidBooking,
 } = require('../controllers/bookingController')
 
 const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', protect, createBooking)
 router.get('/unpaid', protect, unpaidBooking)
+router.post('/paid', protect, paidBooking)
 module.exports = router
