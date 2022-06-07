6e07114b24947522547760989b9a9e77fb4a6251
tonyvu1289 <tonyvu1289@gmail.com>
2022-06-07 15:44:42 +0700

    unpaid booking api

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -26,8 +26,51 @@ const createBooking = asyncHandler(async (req, res) => {
         data:newBooking,
     })
 })
+//@desc get all DonDatPhong
+//@route GET /api/booking/unpaid
+//@access Public
+const unpaidBooking = asyncHandler(async (req, res) => {
+    const idKhach = req.account.CMNDKhach
+    //log infomation
+    console.log(idKhach)
+    //find DonDatPhongs with ngayTT is null
+    const unpaidBooking = await sequelize.query("\
+    select \
+    DonDatPhongs.id,\
+    datediff(DonDatPhongs.ngayKT,DonDatPhongs.ngayBD)*LoaiPhongs.DonGia as price,\
+    LoaiPhongs.Ten as name,\
+    Phongs.id as roomID,\
+    Phongs.GhiChu as info,\
+    LoaiPhongs.image as image,\
+    DonDatPhongs.ngayBD as startDate,\
+    DonDatPhongs.ngayKT as endDate\
+    from \
+        (DonDatPhongs left join \
+        Phongs on DonDatPhongs.idPhong = Phongs.id)\
+        left join \
+        LoaiPhongs on LoaiPhongs.id = Phongs.idLoaiPhong\
+        left join\
+        Khachs on DonDatPhongs.CMNDKhach = Khachs.id\
+        where\
+        DonDatPhongs.ngayTT is null\
+        and Khachs.id = '"+idKhach+"'\
+    ;    \
+    ",{type: sequelize.QueryTypes.SELECT})
+    console.log(unpaidBooking)
+    // total price
+    let totalPrice = 0
+    for(let i=0;i<unpaidBooking.length;i++){
+        totalPrice+=unpaidBooking[i].price
+    }
+    res.status(200).json({
+        success:true,
+        bills:unpaidBooking,
+        totalValue:totalPrice,
+    })
+})
 
 //export
 module.exports = {
     createBooking,
+    unpaidBooking,
 }
--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const {
     createBooking,
+    unpaidBooking,
 } = require('../controllers/bookingController')
 
 const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', protect, createBooking)
-
+router.get('/unpaid', protect, unpaidBooking)
 module.exports = router
